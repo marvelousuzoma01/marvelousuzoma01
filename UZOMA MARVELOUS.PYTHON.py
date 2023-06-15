ff94a7a39296cc4e82d00ac84896e5164b59c136
marvelousuzoma01 <130617409+marvelousuzoma01@users.noreply.github.com>
2023-06-15 08:36:21 +0100

    vsstarter

---
+++ UZOMA MARVELOUS.PYTHON.py
@@ -0,0 +1,13 @@
+Python 3.10.9 (tags/v3.10.9:1dd9be6, Dec  6 2022, 20:01:21) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Marks=int ("enter your marks:
+...            if marks >80
+...            print("grade A")
+...            elif marks>70
+...            print("grade B)
+...                  elif marks>60
+...                  print("grade C"
+...                        elif marks>50
+...                        print("grade D
+...                              elif marks>40
+...                              print("grade F
